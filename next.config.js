c923907a7d1d40bb3b4e2b93b7ae24942f7b45ca
Roberto Ríos Perea <roberto.rios.pere@outlook.com>
2022-04-03 16:03:43 +0200

    feat: add

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   distDir: 'dist',
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/seed-generator/' : '',
 };
