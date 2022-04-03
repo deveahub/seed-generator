78b5d4855fe5320e03a0afae8b99c4e5954ea7da
Roberto Ríos Perea <roberto.rios.pere@outlook.com>
2022-04-03 06:14:58 +0200

    feat: add

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,3 @@
 export { default as darkTheme } from './darkTheme';
 export { default as globalStyles } from './globalStyles';
 export * from './stitches';
-export { default as useThemeVariant } from './useThemeVariant';
